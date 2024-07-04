cf0fca870eeee8e14cac22c33f0b9c459e057e45
Tristen Roupp <tristenr@usf.edu>
2024-07-04 09:57:51 -0400

    Display educational background in a dedicated section

---
+++ my-app/src/components/Education.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Education.css';
+
+const EducationItem = ({ institution, degree, years }) => {
+    return (
+        <div className="education-item">
+            <h3>{institution}</h3>
+            <p>{degree}</p>
+            <p>{years}</p>
+        </div>
+    );
+};
+
+const Education = () => {
+    const education = [
+        { institution: 'University of South Florida', degree: 'B.A. in Business Administration and Information Systems', years: '2020 - 2024' },
+    ];
+
+    return (
+        <div className="education">
+            <h2>Education</h2>
+            {education.map((edu, index) => (
+                <EducationItem key={index} institution={edu.institution} degree={edu.degree} years={edu.years} />
+            ))}
+        </div>
+    );
+};
+
+export default Education;
