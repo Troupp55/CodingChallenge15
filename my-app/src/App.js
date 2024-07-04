9a2fd8a4ffcc48dad72c9b23f4f3de4c612a6333
Tristen Roupp <tristenr@usf.edu>
2024-07-04 09:59:17 -0400

    Integrate all sections into the main App component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import PersonalInfo from './components/PersonalInfo';
+import Skills from './components/Skills';
+import Experience from './components/Experience';
+import Education from './components/Education';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <PersonalInfo />
+            <Skills />
+            <Experience />
+            <Education />
+        </div>
+    );
 }
 
 export default App;
